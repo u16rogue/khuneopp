1b6cb074f90c4f968d585c6391ea328de7b398a6
u16rogue <u16rogue@proton.me>
2023-09-19 16:54:21 +0800

    modified ref_count method for refobj to return a synthetic count, added ref_count_real to refobj

--- include/khpp-astgen/parser.hpp
+++ include/khpp-astgen/parser.hpp
@@ -2,6 +2,10 @@
 
 #if defined(KHPP_ASTGEN) && KHPP_ASTGEN == 1
 
+extern "C" {
+  #include <kh-astgen/parser.h>
+}
+
 namespace khpp::astgen {
 
 } // namespace khpp::astgen
--- include/khpp-core/refobj.hpp
+++ include/khpp-core/refobj.hpp
@@ -12,6 +12,16 @@ using rcb_fnt = kh_refobj_rcb_fnt;
 
 template <typename T> struct irefobj;
 
+/*
+ *  Implements `kh_refobj` used for storing
+ *  a refrence object.
+ *
+ *  NOTE: `refobj` stores its own `refobji` instance.
+ *  At the point where the object is supposed to
+ *  be unreferenced it is normal that the reference
+ *  count is one (1). The destructor or `remove` method
+ *  will handle this.
+ */
 template <typename T>
 struct refobj {
   using self = refobj;
@@ -23,19 +33,44 @@ struct refobj {
     }
   }
 
-  ~refobj() noexcept {
-    if (this->iref != KH_REFOBJ_INVALID_IREF) {
-      kh_refobj_iremove(&this->iref);
+  auto remove() noexcept -> bool {
+    if (
+      (kh_refobj_get_ref_count(this->iref) != 1)
+      ||
+      (this->iref != KH_REFOBJ_INVALID_IREF && kh_refobj_iremove(&this->iref) != KH_TRUE)
+      ||
+      (kh_refobj_uninit(&this->ref) != KH_TRUE)
+    ) {
+      return false;
     }
+
+    return true;
+  }
+
+  ~refobj() noexcept {
+    remove(); 
   }
 
   auto alive() const noexcept -> bool {
     return iref != KH_REFOBJ_INVALID_IREF && kh_refobj_alive(&this->ref) == KH_TRUE;
   }
 
-  auto ref_count() const noexcept -> khpp::u8 {
-    return this->ref._count;
+  /*
+   *  Returns the real reference count of a reference object.
+   *
+   *  NOTE: This does not include its internal reference to
+   *  mirror khuneo's behavior.
+   */
+  auto ref_count_real() const noexcept -> khpp::u8 {
+    return kh_refobj_get_ref_count(this->iref);
   }
+
+  /*
+   *  Returns the reference count of a reference object.
+   */ 
+  auto ref_count() const noexcept -> khpp::u8 {
+    return ref_count_real() - (this->iref != KH_REFOBJ_INVALID_IREF);
+  } 
 
   auto operator->() -> T * {
     return kh_refobj_get_object(this->iref);
@@ -64,6 +99,10 @@ private:
   kh_refobji iref = KH_REFOBJ_INVALID_IREF;
 };
 
+/*
+ *  Implements `kh_refobji` used for representing
+ *  an instance of a reference.
+ */
 template <typename T>
 struct irefobj {
   using self = irefobj<T>;
@@ -135,5 +174,5 @@ private:
   kh_refobji iref = KH_REFOBJ_INVALID_IREF;
 };
 
-} // khpp::refobj
+} // namespace khpp::refobj
 
